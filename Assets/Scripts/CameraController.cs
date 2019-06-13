eea4585574b26c7a0948f4506f4a77a5f4a91b42
Jake Lovrin <jakeop4444@gmail.com>
2019-06-13 16:29:12 -0400

    First commit, camera issues

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public GameObject player;
+    public GameObject target;
+    public GameObject anchor;
+    public float rotate_speed = 5;
+    Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        offset = anchor.transform.position - transform.position;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        float horizontal = Input.GetAxis("Mouse X") * rotate_speed;
+        anchor.transform.Rotate(0, horizontal, 0);
+        player.transform.forward = anchor.transform.forward;
+
+        float desiredAngle = target.transform.eulerAngles.y;
+        Quaternion rotation = Quaternion.Euler(0, desiredAngle, 0);
+        transform.position = target.transform.position - (rotation * offset);
+
+        transform.LookAt(target.transform);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public float speed;
+    public Rigidbody rb;
+    private Vector3 velo;
+    public float jump_speed;
+    public GameObject player;
+
+    void jump()
+    {
+        Vector3 big_jump = new Vector3(0.0f, jump_speed, 0.0f);
+        rb.AddForce(big_jump, ForceMode.VelocityChange);
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 forward = player.transform.forward * moveVertical;
+        Vector3 right = player.transform.right * moveHorizontal;
+
+        Vector3 normalize = (forward + right).normalized;
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+
+        rb.AddForce(normalize * speed * Time.deltaTime);
+
+        velo = rb.velocity;
+
+        velo = Vector3.ClampMagnitude(velo, speed);
+
+        rb.velocity = velo;
+        print(rb.velocity.magnitude);
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            jump();
+        }
+
+
+
+    }
+}
